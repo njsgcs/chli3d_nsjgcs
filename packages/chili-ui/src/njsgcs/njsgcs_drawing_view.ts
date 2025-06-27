2540e6ed15c9531fcb3998c5a4a8a6d17ba9070b
njsgcs <18054661441@163.com>
2025-06-27 17:44:28 +0800

    提出exportdxf

--- packages/chili-ui/src/njsgcs/njsgcs_drawing_view.ts
+++ packages/chili-ui/src/njsgcs/njsgcs_drawing_view.ts
@@ -1,8 +1,8 @@
-import { DxfWriter, point3d } from '@tarikjabiri/dxf';
+
 import { IDocument, Logger, PubSub, ShapeNode } from "chili-core";
 import { getProjectionEdges, gp_Pnt, LineSegmentList, OccShape, ProjectionResult2 } from "chili-wasm";
 import { Matrix3, Vector3 } from "three";
-
+import { export_dxf } from "./njsgcs_export_dxf";
 interface Segment {
     first: gp_Pnt;
     second: gp_Pnt;
@@ -32,55 +32,11 @@ export class njsgcs_drawingView extends HTMLElement {
         });
         PubSub.default.sub("njsgcs_exportdxf", () => {
             Logger.info("njsgcs_export_dxf event triggered");
-            this.export_dxf();
+            export_dxf( this.linelist);
         });
        
     }
-    private export_dxf() {
-   // 如果是浏览器环境
- 
-   const dxf = new DxfWriter();
- 
-  
- 
-    // 添加一个图层（可选）
-    dxf.addLayer('Lines', 7); // 颜色索引 7 是黑色
- 
-  
-    // 遍历 linelist 添加线段
-    for (const [p1x, p1y,p2x,p2y,isHidden] of this.linelist) {
-        const start = point3d(p1x,p1y);
-        const end = point3d(p2x, p2y);
-const CommonEntityOptions = {
-  trueColor: "7",
-     colorNumber: 7,
-     extrusion: undefined,
-     layerName: "Lines",
-     visible: isHidden,
-     lineType: "BYLAYER",
-     lineTypeScale: 1,
- }
-        dxf.addLine(start, end,    CommonEntityOptions ); // 修复图层参数类型为CommonEntityOptions
-    }
- 
- 
-    // 生成 DXF 字符串内容
-    const dxfString = dxf.stringify();
-    
-    // 创建 Blob 并触发下载
-    const blob = new Blob([dxfString], { type: "application/dxf" });
-    const url = URL.createObjectURL(blob);
-    const now = new Date().toISOString()
-    .replace(/[:.]/g, '') // 移除冒号和点
-    .replace('T', '_');
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `${now} output.dxf`;
-    a.click();
- 
-    // 释放资源
-    URL.revokeObjectURL(url);
-}
+   
 
     private drawProjectionEdges(ctx: CanvasRenderingContext2D, projection: ProjectionResult2) {
         // 清除画布
